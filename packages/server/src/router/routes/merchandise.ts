24376cf848629e1e683db9d659438d1b1b56c6c5
dekkerglen <dekker.glen@gmail.com>
2026-05-07 14:14:24 -0400

    Update merchandise.ts

--- packages/server/src/router/routes/merchandise.ts
+++ packages/server/src/router/routes/merchandise.ts
@@ -5,7 +5,7 @@ import { Request, Response } from '../../types/express';
 
 const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2026-04-22.dahlia',
+      apiVersion: '2023-10-16',
     })
   : null;
 
